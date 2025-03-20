2352582d98cb9b5e791ec0661d8eeb8112eef927
Aijin <115892544+Yerbo71@users.noreply.github.com>
2025-03-20 18:05:58 +0500

    feat():auth path

--- src/auth/providers/JWTProvider.tsx
+++ src/auth/providers/JWTProvider.tsx
@@ -12,11 +12,11 @@ import * as authHelper from '../_helpers';
 import { type AuthModel, type UserModel } from '@/auth';
 
 const API_URL = import.meta.env.VITE_APP_API_URL;
-export const LOGIN_URL = `${API_URL}/login`;
+export const LOGIN_URL = `${API_URL}/auth/login`;
 export const REGISTER_URL = `${API_URL}/register`;
 export const FORGOT_PASSWORD_URL = `${API_URL}/forgot-password`;
 export const RESET_PASSWORD_URL = `${API_URL}/reset-password`;
-export const GET_USER_URL = `${API_URL}/get_user`;
+export const GET_USER_URL = `${API_URL}/users/manage`;
 
 interface AuthContextProps {
   loading: boolean;
