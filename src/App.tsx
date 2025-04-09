a983eb71dd9fcab1aee7b2a2ea5c8a502c35a827
Aijin <erbolat.j.2004@gmail.com>
2025-04-09 11:32:58 +0500

    feat():forming route after getCurrentUser

--- src/App.tsx
+++ src/App.tsx
@@ -4,17 +4,24 @@ import { useSettings } from '@/providers/SettingsProvider';
 import { AppRouting } from '@/routing';
 import { PathnameProvider } from '@/providers';
 import { Toaster } from '@/components/ui/sonner';
+import { useAuthContext } from '@/auth';
+import { ScreenLoader } from '@/components';
 
 const { BASE_URL } = import.meta.env;
 
 const App = () => {
   const { settings } = useSettings();
+  const { currentUser } = useAuthContext();
 
   useEffect(() => {
     document.documentElement.classList.remove('dark');
     document.documentElement.classList.remove('light');
     document.documentElement.classList.add(settings.themeMode);
   }, [settings]);
+
+  if (currentUser === undefined) {
+    return <ScreenLoader />;
+  }
 
   return (
     <BrowserRouter
