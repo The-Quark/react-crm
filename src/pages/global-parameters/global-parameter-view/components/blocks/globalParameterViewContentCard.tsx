b8a57f35f10395d8003ff8bed5cc1853dbb0b3b5
Aijin <erbolat.j.2004@gmail.com>
2025-04-11 13:42:23 +0500

    feat():view

---
+++ src/pages/global-parameters/global-parameter-view/components/blocks/globalParameterViewContentCard.tsx
@@ -0,0 +1,65 @@
+import { FC } from 'react';
+import { ParametersModel } from '@/api/getGlobalParameters/types.ts';
+
+interface IGeneralSettingsProps {
+  title: string;
+  parameter: ParametersModel | null;
+}
+
+export const GlobalParameterViewContentCard: FC<IGeneralSettingsProps> = ({ title, parameter }) => {
+  return (
+    <form className="card pb-2.5" noValidate>
+      <div className="card-header" id="general_settings">
+        <h3 className="card-title">{title}</h3>
+      </div>
+      <div className="card-body grid gap-5">
+        <div className="flex items-baseline flex-wrap lg:flex-nowrap gap-2.5">
+          <label className="form-label max-w-56">Company Name</label>
+          <div className="flex columns-1 w-full flex-wrap">{parameter?.company_name}</div>
+        </div>
+
+        <div className="flex items-baseline flex-wrap lg:flex-nowrap gap-2.5">
+          <label className="form-label max-w-56">Time zone</label>
+          <div className="flex columns-1 w-full flex-wrap">{parameter?.timezone}</div>
+        </div>
+
+        <div className="flex items-baseline flex-wrap lg:flex-nowrap gap-2.5">
+          <label className="form-label max-w-56">Currency</label>
+          <div className="flex columns-1 w-full flex-wrap">{parameter?.currency}</div>
+        </div>
+
+        <div className="flex items-baseline flex-wrap lg:flex-nowrap gap-2.5">
+          <label className="form-label max-w-56">Language</label>
+          <div className="flex columns-1 w-full flex-wrap">{parameter?.language}</div>
+        </div>
+
+        <div className="flex items-baseline flex-wrap lg:flex-nowrap gap-2.5">
+          <label className="form-label max-w-56">Legal Address</label>
+          <div className="flex columns-1 w-full flex-wrap">{parameter?.legal_address}</div>
+        </div>
+
+        <div className="flex items-baseline flex-wrap lg:flex-nowrap gap-2.5">
+          <label className="form-label max-w-56">Warehouse Address</label>
+          <div className="flex columns-1 w-full flex-wrap">{parameter?.warehouse_address}</div>
+        </div>
+
+        <div className="flex items-baseline flex-wrap lg:flex-nowrap gap-2.5">
+          <label className="form-label max-w-56">Airlines</label>
+          <div className="flex columns-1 w-full flex-wrap">{parameter?.airlines}</div>
+        </div>
+
+        <div className="flex items-baseline flex-wrap lg:flex-nowrap gap-2.5">
+          <label className="form-label max-w-56">Dimension Per Place</label>
+          <div className="flex columns-1 w-full flex-wrap">{parameter?.dimensions_per_place}</div>
+        </div>
+
+        <div className="flex items-baseline flex-wrap lg:flex-nowrap gap-2.5">
+          <label className="form-label max-w-56">Cost Per Airplace</label>
+          <div className="flex columns-1 w-full flex-wrap">{parameter?.cost_per_airplace}</div>
+        </div>
+      </div>
+    </form>
+  );
+};
+
+export { type IGeneralSettingsProps };
--- src/pages/global-parameters/global-parameter-view/globalParameterViewPage.tsx
+++ src/pages/global-parameters/global-parameter-view/globalParameterViewPage.tsx
@@ -1,5 +1,37 @@
-import React from 'react';
+import React, { Fragment } from 'react';
+import { Container } from '@/components';
+import {
+  Toolbar,
+  ToolbarActions,
+  ToolbarDescription,
+  ToolbarHeading,
+  ToolbarPageTitle
+} from '@/partials/toolbar';
+import { GlobalParameterViewContent } from '@/pages/global-parameters/global-parameter-view/components/globalParamaterViewContent.tsx';
 
 export const GlobalParameterViewPage = () => {
-  return <div>GlobalParameterViewPage</div>;
+  return (
+    <Fragment>
+      <Container>
+        <Toolbar>
+          <ToolbarHeading>
+            <ToolbarPageTitle />
+            <ToolbarDescription>Test description</ToolbarDescription>
+          </ToolbarHeading>
+          <ToolbarActions>
+            <a href="#" className="btn btn-sm btn-light">
+              Test Button
+            </a>
+            <a href="#" className="btn btn-sm btn-primary">
+              Test Button
+            </a>
+          </ToolbarActions>
+        </Toolbar>
+      </Container>
+
+      <Container>
+        <GlobalParameterViewContent />
+      </Container>
+    </Fragment>
+  );
 };
