e0b2135d98e621651613f28c65efb679a61f521a
Aijin <erbolat.j.2004@gmail.com>
2025-04-15 16:11:04 +0500

    feat():some

--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,14 @@
 import '@/components/keenicons/assets/styles.css';
 import './styles/globals.css';
-
 import axios from 'axios';
 import ReactDOM from 'react-dom/client';
-
 import { App } from './App';
 import { setupAxios } from './auth';
 import { ProvidersWrapper } from './providers';
 import React from 'react';
 
-/**
- * Inject interceptors for axios.
- *
- * @see https://github.com/axios/axios#interceptors
+/*
+ Inject interceptors for axios.
  */
 setupAxios(axios);
 
