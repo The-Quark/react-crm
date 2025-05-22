7fdf8ccd53198d8df8fac5823fd7361ce67bf124
Aijin <erbolat.j.2004@gmail.com>
2025-05-22 13:27:46 +0500

    feat():remove unused components

--- src/layouts/demo1/header/HeaderTopbar.tsx
+++ src/layouts/demo1/header/HeaderTopbar.tsx
@@ -31,88 +31,88 @@ const HeaderTopbar = () => {
 
   return (
     <div className="flex items-center gap-2 lg:gap-3.5">
-      <button
-        onClick={handleOpen}
-        className="btn btn-icon btn-icon-lg size-9 rounded-full hover:bg-primary-light hover:text-primary text-gray-500"
-      >
-        <KeenIcon icon="magnifier" />
-      </button>
-      <ModalSearch open={searchModalOpen} onOpenChange={handleClose} />
+      {/*<button*/}
+      {/*  onClick={handleOpen}*/}
+      {/*  className="btn btn-icon btn-icon-lg size-9 rounded-full hover:bg-primary-light hover:text-primary text-gray-500"*/}
+      {/*>*/}
+      {/*  <KeenIcon icon="magnifier" />*/}
+      {/*</button>*/}
+      {/*<ModalSearch open={searchModalOpen} onOpenChange={handleClose} />*/}
 
-      <Menu>
-        <MenuItem
-          ref={itemChatRef}
-          onShow={handleShow}
-          toggle="dropdown"
-          trigger="click"
-          dropdownProps={{
-            placement: isRTL() ? 'bottom-start' : 'bottom-end',
-            modifiers: [
-              {
-                name: 'offset',
-                options: {
-                  offset: isRTL() ? [-170, 10] : [170, 10]
-                }
-              }
-            ]
-          }}
-        >
-          <MenuToggle className="btn btn-icon btn-icon-lg size-9 rounded-full hover:bg-primary-light hover:text-primary dropdown-open:bg-primary-light dropdown-open:text-primary text-gray-500">
-            <KeenIcon icon="messages" />
-          </MenuToggle>
+      {/*<Menu>*/}
+      {/*  <MenuItem*/}
+      {/*    ref={itemChatRef}*/}
+      {/*    onShow={handleShow}*/}
+      {/*    toggle="dropdown"*/}
+      {/*    trigger="click"*/}
+      {/*    dropdownProps={{*/}
+      {/*      placement: isRTL() ? 'bottom-start' : 'bottom-end',*/}
+      {/*      modifiers: [*/}
+      {/*        {*/}
+      {/*          name: 'offset',*/}
+      {/*          options: {*/}
+      {/*            offset: isRTL() ? [-170, 10] : [170, 10]*/}
+      {/*          }*/}
+      {/*        }*/}
+      {/*      ]*/}
+      {/*    }}*/}
+      {/*  >*/}
+      {/*    <MenuToggle className="btn btn-icon btn-icon-lg size-9 rounded-full hover:bg-primary-light hover:text-primary dropdown-open:bg-primary-light dropdown-open:text-primary text-gray-500">*/}
+      {/*      <KeenIcon icon="messages" />*/}
+      {/*    </MenuToggle>*/}
 
-          {DropdownChat({ menuTtemRef: itemChatRef })}
-        </MenuItem>
-      </Menu>
+      {/*    {DropdownChat({ menuTtemRef: itemChatRef })}*/}
+      {/*  </MenuItem>*/}
+      {/*</Menu>*/}
 
-      <Menu>
-        <MenuItem
-          ref={itemAppsRef}
-          toggle="dropdown"
-          trigger="click"
-          dropdownProps={{
-            placement: isRTL() ? 'bottom-start' : 'bottom-end',
-            modifiers: [
-              {
-                name: 'offset',
-                options: {
-                  offset: isRTL() ? [-10, 10] : [10, 10]
-                }
-              }
-            ]
-          }}
-        >
-          <MenuToggle className="btn btn-icon btn-icon-lg size-9 rounded-full hover:bg-primary-light hover:text-primary dropdown-open:bg-primary-light dropdown-open:text-primary text-gray-500">
-            <KeenIcon icon="element-11" />
-          </MenuToggle>
+      {/*<Menu>*/}
+      {/*  <MenuItem*/}
+      {/*    ref={itemAppsRef}*/}
+      {/*    toggle="dropdown"*/}
+      {/*    trigger="click"*/}
+      {/*    dropdownProps={{*/}
+      {/*      placement: isRTL() ? 'bottom-start' : 'bottom-end',*/}
+      {/*      modifiers: [*/}
+      {/*        {*/}
+      {/*          name: 'offset',*/}
+      {/*          options: {*/}
+      {/*            offset: isRTL() ? [-10, 10] : [10, 10]*/}
+      {/*          }*/}
+      {/*        }*/}
+      {/*      ]*/}
+      {/*    }}*/}
+      {/*  >*/}
+      {/*    <MenuToggle className="btn btn-icon btn-icon-lg size-9 rounded-full hover:bg-primary-light hover:text-primary dropdown-open:bg-primary-light dropdown-open:text-primary text-gray-500">*/}
+      {/*      <KeenIcon icon="element-11" />*/}
+      {/*    </MenuToggle>*/}
 
-          {DropdownApps()}
-        </MenuItem>
-      </Menu>
+      {/*    {DropdownApps()}*/}
+      {/*  </MenuItem>*/}
+      {/*</Menu>*/}
 
-      <Menu>
-        <MenuItem
-          ref={itemNotificationsRef}
-          toggle="dropdown"
-          trigger="click"
-          dropdownProps={{
-            placement: isRTL() ? 'bottom-start' : 'bottom-end',
-            modifiers: [
-              {
-                name: 'offset',
-                options: {
-                  offset: isRTL() ? [-70, 10] : [70, 10] // [skid, distance]
-                }
-              }
-            ]
-          }}
-        >
-          <MenuToggle className="btn btn-icon btn-icon-lg relative cursor-pointer size-9 rounded-full hover:bg-primary-light hover:text-primary dropdown-open:bg-primary-light dropdown-open:text-primary text-gray-500">
-            <KeenIcon icon="notification-status" />
-          </MenuToggle>
-          {DropdownNotifications({ menuTtemRef: itemNotificationsRef })}
-        </MenuItem>
-      </Menu>
+      {/*<Menu>*/}
+      {/*  <MenuItem*/}
+      {/*    ref={itemNotificationsRef}*/}
+      {/*    toggle="dropdown"*/}
+      {/*    trigger="click"*/}
+      {/*    dropdownProps={{*/}
+      {/*      placement: isRTL() ? 'bottom-start' : 'bottom-end',*/}
+      {/*      modifiers: [*/}
+      {/*        {*/}
+      {/*          name: 'offset',*/}
+      {/*          options: {*/}
+      {/*            offset: isRTL() ? [-70, 10] : [70, 10] // [skid, distance]*/}
+      {/*          }*/}
+      {/*        }*/}
+      {/*      ]*/}
+      {/*    }}*/}
+      {/*  >*/}
+      {/*    <MenuToggle className="btn btn-icon btn-icon-lg relative cursor-pointer size-9 rounded-full hover:bg-primary-light hover:text-primary dropdown-open:bg-primary-light dropdown-open:text-primary text-gray-500">*/}
+      {/*      <KeenIcon icon="notification-status" />*/}
+      {/*    </MenuToggle>*/}
+      {/*    {DropdownNotifications({ menuTtemRef: itemNotificationsRef })}*/}
+      {/*  </MenuItem>*/}
+      {/*</Menu>*/}
 
       <Menu>
         <MenuItem
