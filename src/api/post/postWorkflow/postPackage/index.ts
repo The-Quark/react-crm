14b3b2646a3bc4691a855f243fe373f604da6b5a
Aijin <erbolat.j.2004@gmail.com>
2025-07-01 15:16:42 +0500

    feat():some

--- src/api/post/postWorkflow/postPackage/index.ts
+++ src/api/post/postWorkflow/postPackage/index.ts
@@ -1,16 +1,14 @@
 import axios from 'axios';
 import { IPackageFormValues } from '@/api/post/postWorkflow/postPackage/types.ts';
 import { PACKAGE_URL } from '@/api/url';
-import { cleanValues } from '@/utils/lib/helpers.ts';
 
 export const postPackage = async (
   data: Omit<IPackageFormValues, 'id' | 'created_at' | 'updated_at'>
 ): Promise<IPackageFormValues> => {
-  const cleanedData = cleanValues(data);
   return await axios
     .post<IPackageFormValues>(
       PACKAGE_URL,
-      { ...cleanedData, frontcrm: true },
+      { ...data, frontcrm: true },
       {
         headers: { 'Content-Type': 'application/json' }
       }
