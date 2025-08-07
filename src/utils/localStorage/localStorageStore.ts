969c92f4f9776858e63c3ef12c2cf72cfe7aa1d7
Aijin <erbolat.j.2004@gmail.com>
2025-08-07 15:50:37 +0500

    feat():some locales changes

--- src/utils/localStorage/localStorageStore.ts
+++ src/utils/localStorage/localStorageStore.ts
@@ -1,2 +1,4 @@
-export const LOCAL_STORAGE_CURRENCY_KEY = 'app_currency';
-export const I18N_CONFIG_KEY = 'i18nConfig';
+const APP_PREFIX = `${import.meta.env.VITE_APP_NAME}-app-v${import.meta.env.VITE_APP_VERSION}-dev-`;
+
+export const LOCAL_STORAGE_CURRENCY_KEY = `${APP_PREFIX}app_currency`;
+export const I18N_CONFIG_KEY = `${APP_PREFIX}i18nConfig`;
